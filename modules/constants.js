90aa67e8ac048ab8a7cc5805699856dbe290882c
Zack Stickles <zstickles@newrelic.com>
2022-01-25 16:41:48 -0800

    chore: typedef and helper function

--- modules/constants.js
+++ modules/constants.js
@@ -4,3 +4,9 @@ export const MODULE_SETTINGS = {
   ACCOUNT_ID: "account-id",
   LICENSE_KEY: "license-key",
 };
+
+export const EVENT_TYPE = "FoundryVTTEvent";
+
+export const EVENT_NAMES = {
+  ROLL: "roll",
+};
--- modules/trackEvent.js
+++ modules/trackEvent.js
@@ -1,8 +1,27 @@
 import { readSetting } from "./settings.js";
 import Logger from "./utils/logger.js";
-import { MODULE_SETTINGS } from "./constants.js";
+import { EVENT_TYPE, MODULE_SETTINGS } from "./constants.js";
 
 const API_BASE_URL = "https://insights-collector.newrelic.com/v1";
+
+/**
+ * Data to send to New Relic event API.
+ *
+ * @typedef {Object} FoundryVTTEvent
+ * @property {EVENT_TYPE} eventType The event type (always the same)
+ * @property {string} eventName The name of the event (e.g. "roll")
+ * @property {string} world The ID of the game world
+ * @property {string} system The ID of the game system (e.g. dnd5e)
+ *
+ * @property {string} [rollType] (Optional) The type of roll (e.g. attack, saving throw)
+ * @property {string} [rollRquest] (Optional) What was attempted to be roll (e.g. 2d6)
+ * @property {number} [rollResult] (Optional) The final result of the roll (e.g. 17)
+ * @property {boolean} [rollHasAdvantage]
+ * @property {boolean} [rollHasDisadvantage]
+ *
+ * @property {string} [actorName] (Optional) Name of actor that triggered the event
+ * @property {string} [actorHealth] (Optional) Current health of the actor (if applicable)
+ */
 
 /**
  * Gets the account-specific API URL for use in API requests.
@@ -15,12 +34,12 @@ const _getAPIURL = (accountID) => `${API_BASE_URL}/${accountID}/events`;
 /**
  * Makes a request to the New Relic events API.
  *
- * @param {Object} body Event data to send to New Relic
+ * @param {FoundryVTTEvent} body Event data to send to New Relic
+ * @param {string} accountID The account to send the event data to
+ * @param {string} licenseKey The New Relic license key for data ingest
  * @returns {Promise<void>}
  */
-const _makeAPIRequest = async (body) => {
-  const licenseKey = readSetting(MODULE_SETTINGS.LICENSE_KEY);
-  const accountID = readSetting(MODULE_SETTINGS.ACCOUNT_ID);
+const _makeAPIRequest = async (body, accountID, licenseKey) => {
   const url = _getAPIURL(accountID);
 
   try {
@@ -41,5 +60,29 @@ const _makeAPIRequest = async (body) => {
   }
 };
 
-// TODO: typedef for an event
-// TODO: export function for tracking event (that calls above func)
+/**
+ * Verifies that a license key and account ID have been set and then
+ * constructs an event to send to the API.
+ *
+ * @param {Object} data Event data to send to New Relic
+ * @param {string} data.eventName The name of the event (required)
+ * @returns {Promise<void>}
+ */
+export const track = async (data) => {
+  const licenseKey = readSetting(MODULE_SETTINGS.LICENSE_KEY);
+  const accountID = readSetting(MODULE_SETTINGS.ACCOUNT_ID);
+
+  if (!licenseKey || !accountID) {
+    Logger.warn("License key and Account ID are required");
+    return;
+  }
+
+  const body = {
+    eventType: EVENT_TYPE,
+    world: game.world.id,
+    system: game.system.id,
+    ...data,
+  };
+
+  await _makeAPIRequest(body, accountID, licenseKey);
+};
