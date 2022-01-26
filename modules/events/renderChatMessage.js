40cf2a69ee8c6f6c2980d860ad4e8030f0f47463
Zack Stickles <zstickles@newrelic.com>
2022-01-26 13:37:30 -0800

    chore: only track roll chat messages for now

--- modules/events/renderChatMessage.js
+++ modules/events/renderChatMessage.js
@@ -7,11 +7,14 @@ import { EVENT_NAMES } from "../constants.js";
 /**
  * @typedef {Object} Message
  * @property {import('../utils/getRollDetails').Roll} roll
- * @property {string} alias The character name
  * @property {import('../utils/getUserDetails').User} user The user who produced the message
+ * @property {string} alias The character name
+ * @property {boolean} isRoll Whether or not the message represents a roll
  */
 
 /**
+ * @todo Handle different types of chat events
+ *
  * @param {Message} message Message details
  * @param {Object} [_html] HTML for the message (not currently used)
  * @param {Object} [_data] Theinput data provided (not currently used)
@@ -20,12 +23,12 @@ import { EVENT_NAMES } from "../constants.js";
 export default async (message, _html, _data) => {
   if (!isGM()) return;
 
-  /** @todo Handle different types of chat events */
-
-  return trackEvent({
-    eventName: EVENT_NAMES.ROLL,
-    userCharacter: message.alias,
-    ...getRollDetails(message.roll),
-    ...getUserDetails(message.user),
-  });
+  if (message.isRoll) {
+    return trackEvent({
+      eventName: EVENT_NAMES.ROLL,
+      userCharacter: message.alias,
+      ...getRollDetails(message.roll),
+      ...getUserDetails(message.user),
+    });
+  }
 };
