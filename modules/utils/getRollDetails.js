504a17f31daf373af3c9315536921b2a534b5875
Zack Stickles <zstickles@newrelic.com>
2022-01-26 17:21:14 -0800

    chore: extracting d20 results for logging

--- modules/utils/getRollDetails.js
+++ modules/utils/getRollDetails.js
@@ -1,11 +1,29 @@
 /**
+ *
+ * @typedef {Object} Die
+ * @property {number} faces
+ * @property {number} total
+ */
+
+/**
  * @typedef {Object} Roll
  * @property {string} formula The roll formula used (e.g. "1d20+3")
  * @property {number} total The total result for the roll
+ * @property {Die[]} dice The die/dice used for this roll
  * @property {boolean} hasAdvantage
  * @property {boolean} hasDisadvantage
  * @property {{ flavor: string }} options
  */
+
+/**
+ * Gets the value of the d20 used for this roll, if applicable.
+ *
+ * @todo Evaluate if this is the right approach
+ *
+ * @param {Die[]} dice The dice used for this roll
+ * @returns {?number} If a d20, this will return the number rolled (before modifiers)
+ */
+const _getD20Roll = (dice) => dice.find((d) => d.faces === 20)?.total;
 
 /**
  * Gets the roll type from the roll "flavor" provided by the platform.
@@ -23,7 +41,8 @@ const _getRollType = ({ options }) => options.flavor?.split(":")?.[0];
 export default (roll) => ({
   rollType: _getRollType(roll),
   rollRequest: roll.formula,
-  rollResult: roll.total,
+  rollResult: _getD20Roll(roll),
+  rollTotal: roll.total,
   rollHasAdvantage: roll.hasAdvantage,
   rollHasDisadvantage: roll.hasDisadvantage,
 });
